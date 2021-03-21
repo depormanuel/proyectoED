ed6a0eb93cfb24e6cea71df0bc9db07d31fdafb4
Manuel <depormanuel@gmail.com>
2021-03-21 23:27:05 +0100

    Clase TDS comentada

---
+++ ThreeDimensionalShape.java
@@ -0,0 +1,33 @@
+package proyectoED;
+
+
+/**
+ * Esta es la clase threeDimensionalShape , representa area y volumen de esas clases.
+ * de esta clase heredan otras.
+ * 
+ * @author  Manuel Aranda Perez
+ * @version 2.0 Date: 21/03/2021
+ */
+
+abstract class ThreeDimensionalShape extends Shape{
+ 
+    /**
+     * Devuelve un 0, esta clase se sobreescribira.
+     * Devuelve el valor del area de superficie.
+     */
+    public double getSurfaceArea()
+    {
+        return 0 ;
+    }
+    
+    
+    /**
+     * Devuelve un 0, esta clase se sobreescribira.
+     * Devuelve el valor del volumen.
+     */
+    public double getVolume()
+    {
+        return 0;
+    }
+    
+}
